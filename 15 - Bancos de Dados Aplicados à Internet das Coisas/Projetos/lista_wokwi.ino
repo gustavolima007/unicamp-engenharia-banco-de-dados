900422f6729618b0752a22ef0e8fa61b57dd0c98
Gustavo Lima <adm.gustavo.lima@gmail.com>
2025-07-15 20:50:19 -0300

    Incluindo lista Wokwi 3

---
+++ 15 - Bancos de Dados Aplicados à Internet das Coisas/Projetos/lista_wokwi.ino
@@ -0,0 +1,58 @@
+ UNICAMP - Wokwi
+-------------------------------------------------------------------
+Projeto  -  link
+Prog00   - UNO https://wokwi.com/projects/433665359663475713 NANO https://wokwi.com/projects/433668238426445825 ESP32 https://wokwi.com/projects/433673389004990465
+Prog01   - UNO https://wokwi.com/projects/433665370301280257 NANO https://wokwi.com/projects/433668246016040961 ESP32 https://wokwi.com/projects/435660603045421057
+Prog02   - UNO https://wokwi.com/projects/433665380413748225 NANO https://wokwi.com/projects/433668262555232257 ESP32 https://wokwi.com/projects/435662443798666241
+Prog03   - UNO https://wokwi.com/projects/433665393883757569 NANO https://wokwi.com/projects/433668271604445185 ESP32 https://wokwi.com/projects/435662264399887361
+Prog04   - UNO https://wokwi.com/projects/433665403455160321 NANO https://wokwi.com/projects/433668281816528897 ESP32 https://wokwi.com/projects/435661966408208385
+Prog05   - UNO https://wokwi.com/projects/433665416028637185 NANO https://wokwi.com/projects/433668291534732289 ESP32 https://wokwi.com/projects/435659089846265857
+Prog06   - UNO https://wokwi.com/projects/433665880397904897 NANO https://wokwi.com/projects/433668510111464449 ESP32 https://wokwi.com/projects/435662985829663745
+Prog07   - UNO https://wokwi.com/projects/433665890320581633 NANO https://wokwi.com/projects/433668518923698177 ESP32 https://wokwi.com/projects/435659171720131585
+Prog08   - UNO https://wokwi.com/projects/433665901204801537 NANO https://wokwi.com/projects/433668527940407297 ESP32 Aguarde ...
+Prog09   - UNO https://wokwi.com/projects/433665913197366273 NANO https://wokwi.com/projects/433668537393323009 ESP32 Aguarde ...
+Prog10   - UNO https://wokwi.com/projects/433665925451028481 NANO https://wokwi.com/projects/433668546689998849 ESP32 Aguarde ...
+Prog11   - UNO https://wokwi.com/projects/433666194737959937 NANO https://wokwi.com/projects/433668780311700481 ESP32 Aguarde ...
+Prog12   - UNO https://wokwi.com/projects/433666204514883585 NANO https://wokwi.com/projects/433668790221795329 ESP32 https://wokwi.com/projects/435663822540939265
+Prog13   - UNO https://wokwi.com/projects/433666213243231233 NANO https://wokwi.com/projects/433668798405933057 ESP32 https://wokwi.com/projects/435663964977429505
+Prog14   - UNO https://wokwi.com/projects/433666226216216577 NANO https://wokwi.com/projects/433668808072756225 ESP32 https://wokwi.com/projects/435664263559469057
+Prog15   - UNO https://wokwi.com/projects/433666235933371393 NANO https://wokwi.com/projects/433668817317004289 ESP32 https://wokwi.com/projects/435659292071508993
+Prog16   - UNO https://wokwi.com/projects/433666557031508993 NANO https://wokwi.com/projects/433669027876327425 ESP32 Aguarde ...
+Prog17   - UNO https://wokwi.com/projects/433666565795510273 NANO https://wokwi.com/projects/433669038926222337 ESP32 https://wokwi.com/projects/435659404090887169
+Prog18   - UNO https://wokwi.com/projects/433666574343502849 NANO https://wokwi.com/projects/433669047675542529 ESP32 https://wokwi.com/projects/435664496441926657
+Prog19   - UNO https://wokwi.com/projects/433666585275959297 NANO https://wokwi.com/projects/433669056238215169 ESP32 https://wokwi.com/projects/435659434862400513
+Prog20   - UNO https://wokwi.com/projects/433666542137534465 NANO https://wokwi.com/projects/433669064199005185 ESP32 Aguarde ...
+Prog21   - UNO https://wokwi.com/projects/433666876854553601 NANO https://wokwi.com/projects/433669347166702593 ESP32 https://wokwi.com/projects/435659716652554241
+Prog22   - UNO https://wokwi.com/projects/433666886895718401 NANO https://wokwi.com/projects/433669356669948929 ESP32 https://wokwi.com/projects/435659709824224257
+Prog23   - UNO https://wokwi.com/projects/433666897247264769 NANO https://wokwi.com/projects/433669365414029313 ESP32 https://wokwi.com/projects/435659729965279233
+Prog24   - UNO https://wokwi.com/projects/433666907240195073 NANO https://wokwi.com/projects/433669373943147521 ESP32 https://wokwi.com/projects/435659722774142977
+
+Prog25   - Sem Equivalência no momento
+Prog26   - Sem Equivalência no momento
+
+Prog27   - UNO https://wokwi.com/projects/433667135838180353 NANO https://wokwi.com/projects/433669587651341313 ESP32 Aguarde ...
+Prog28   - UNO https://wokwi.com/projects/433667145397000193 NANO https://wokwi.com/projects/433669598828114945 ESP32 Aguarde ...
+Prog29   - UNO https://wokwi.com/projects/433667154555264001 NANO https://wokwi.com/projects/433669606892713985 ESP32 https://wokwi.com/projects/435659735223890945
+Prog30   - UNO https://wokwi.com/projects/433667165221380097 NANO https://wokwi.com/projects/433669614758084609 ESP32 Aguarde ...
+Prog31   - UNO https://wokwi.com/projects/433667183874500609 NANO https://wokwi.com/projects/433669623734945793 ESP32 https://wokwi.com/projects/435659740153248769
+
+Prog32   - Sem Equivalência no momento
+Prog33   - Sem Equivalência no momento
+Prog34   - Sem Equivalência no momento
+Prog35   - Sem Equivalência no momento
+Prog36   - Sem Equivalência no momento
+
+Prog37   - UNO https://wokwi.com/projects/433667528083237889 NANO https://wokwi.com/projects/433669833675626497 ESP32 https://wokwi.com/projects/435660110876283905
+Prog38   - UNO https://wokwi.com/projects/433667538185223169 NANO https://wokwi.com/projects/433669850233693185 ESP32 https://wokwi.com/projects/435660116845829121
+Prog39   - UNO https://wokwi.com/projects/433667912184010753 NANO https://wokwi.com/projects/433669860853672961 ESP32 https://wokwi.com/projects/435665028914105345
+Prog40   - UNO https://wokwi.com/projects/433667560011337729 NANO https://wokwi.com/projects/433669871174808577 ESP32 Aguarde ...
+
+Prog41   - Sem Equivalência no momento
+Prog42   - Sem Equivalência no momento
+Prog43   - Sem Equivalência no momento
+Prog44   - Sem Equivalência no momento
+
+Entradas Analógicas  - ESP32 https://wokwi.com/projects/433673516712123393
+Entradas Digitais    - ESP32 https://wokwi.com/projects/433673660843097089
+Saídas Digitais      - ESP32 https://wokwi.com/projects/433673685081980929
+Saídas Analógicas    - ESP32 https://wokwi.com/projects/433673707569742849
